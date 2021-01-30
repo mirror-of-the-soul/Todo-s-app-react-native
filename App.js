07749ac3ba6d1700f3aa1f31c35a80b2656f0f52
mirror-of-the-soul <grigar19@gmail.com>
2021-01-30 21:15:35 +0300

    create state + forms

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {Navbar} from './src/Navbar';
+import {AddTodo} from './src/AddTodo';
+import {Todo} from './src/Todo';
 
 export default function App() {
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (title) => {
+    // const newTodo = {
+    //   id: Date.now().toString(),
+    //   title: title
+    // }
+
+    // setTodos(todos.concat([newTodo]))
+
+    // setTodos((prevTodos) => {
+    //   return [
+    //     ...prevTodos,
+    //     newTodo
+    //   ]
+    // })
+
+    setTodos(prev => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title
+      }
+    ])
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Navbar title='Todo App!'/>
+      <View style={styles.container}>
+        <AddTodo onSubmit={addTodo}/>
+
+        <View>
+          { todos.map(todo => (
+            <Todo todo={todo} key={todo.id} />
+          )) }
+        </View>
+
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    paddingHorizontal: 30,
+    paddingVertical: 20
+  }
+})
